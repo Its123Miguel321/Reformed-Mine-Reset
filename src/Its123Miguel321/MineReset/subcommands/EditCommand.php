eadb96a8e36802d444e44ac0516ecd9c39c178ee
Its123Miguel321 <77432615+Its123Miguel321@users.noreply.github.com>
2023-02-07 17:58:09 -0800

    Update EditCommand.php

--- src/Its123Miguel321/MineReset/subcommands/EditCommand.php
+++ src/Its123Miguel321/MineReset/subcommands/EditCommand.php
@@ -1,15 +1,14 @@
 <?php
 
-namespace ReformedDevs\Prisons_Core\Core\plugins\MineReset\subcommands;
+namespace Its123Miguel321\MineReset\subcommands;
 
 use pocketmine\command\CommandSender;
 use pocketmine\item\StringToItemParser;
 use pocketmine\player\Player;
 use pocketmine\utils\TextFormat as TF;
-use ReformedDevs\Prisons_Core\Core\plugins\MineReset\ResetSettings;
-use ReformedDevs\Prisons_Core\Core\plugins\MineReset\subcommands\SubCommand;
-use ReformedDevs\Prisons_Core\Core\plugins\SellSystem\settings\SellSettings;
-use ReformedDevs\Prisons_Core\Core\Utils\ServerUtils;
+
+use Its123Miguel321\MineReset\ResetSettings;
+use Its123Miguel321\MineReset\subcommands\SubCommand;
 
 class EditCommand extends SubCommand
 {
@@ -49,7 +48,7 @@ class EditCommand extends SubCommand
 
         if(is_null($mine))
         {
-            $sender->sendMessage(ServerUtils::ERROR . TF::DARK_GRAY . $args[0] . TF::GRAY . ' is not a mine!');
+            $sender->sendMessage(TF::DARK_GRAY . $args[0] . TF::GRAY . ' is not a mine!');
             return false;
         }
 
@@ -57,7 +56,7 @@ class EditCommand extends SubCommand
 
         $mine->setName($args[1]);
 
-        $sender->sendMessage(ServerUtils::SUCCESS . TF::GRAY . 'You changed mine ' . TF::DARK_GRAY . $orignal . TF::GRAY . ' to ' . TF::GOLD . $mine->getName() . TF::GRAY . '!');
+        $sender->sendMessage(TF::GRAY . 'You changed mine ' . TF::DARK_GRAY . $orignal . TF::GRAY . ' to ' . TF::GOLD . $mine->getName() . TF::GRAY . '!');
 		return true;
 	}
 }
